e514af70db27663a4c737d2c8a2345bd57ce2adc
조용구 <younygo1004@gmail.com>
2024-02-17 01:49:03 +0900

    다음 블로그 글에 사용된 Code Snippet 추가 ArrayList의 default constructor

---
+++ 프로그래밍 언어/java/ArrayList의 default constructor/ArrayListTest.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class ArrayListTest {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> testList = new ArrayList<>();
+
+        System.out.println();
+
+        testList.add("test");
+
+        System.out.println();
+
+    }
+
+}
